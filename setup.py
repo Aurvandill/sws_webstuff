8fa116d2f118a5353b6f26eeca32e7868fd0cdc5
Aurvandill137 <cerberus885@gmail.com>
2022-03-22 17:33:54 +0100

    updated setup and version

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from sws_webstuff import __version__
 setup(
 
-    version=__version__
+    version=__version__,
+    packages = find_packages()
 )
